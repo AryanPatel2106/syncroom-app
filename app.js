e65137bf0bc7f0f6f69df3afb46283d3d5ebd6cf
AryanPatel2106 <aryanpatel8082@gmail.com>
2025-08-14 21:29:33 +0530

    Feat: Add animations, dark mode, and password validation

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const multer = require('multer');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-const PORT = process.env.PORT || 5000;
-
+const PORT = 5000;
 
 // Multer config
 const storage = multer.diskStorage({
